bf70bec1ebab6ee4a1ef173ec036d71e51fedb6f
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2025-08-17 22:12:48 -0500

    fix: adjust middleware regex to stop endless login loop

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
+     * - /login (login page)
+     * - /auth (authentication routes)
+     * - /error (error page)
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|login|auth|error|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
